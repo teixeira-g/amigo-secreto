dba31fda03eaeb1d207bca3617789fd52bba7e08
teixeira-g <gabriel.txr00@gmail.com>
2025-02-02 20:13:58 -0300

    Funcionalidade de adicionar amigo a lista de array criada

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo (){
+    let input = document.querySelector('input');
+    if (input.value == ''){
+        alert('Por favor, insira um nome!');
+    } else {
+        amigos.push(input.value);
+        input.value = '';
+        input.focus();
+    }
+
+    console.log(amigos);
+}
