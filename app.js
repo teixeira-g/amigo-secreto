bdc02b3e43d6201855e9cacd917d7629aecc0005
teixeira-g <gabriel.txr00@gmail.com>
2025-02-03 11:48:15 -0300

    Função de adicionarAmigo finalizada

--- app.js
+++ app.js
@@ -11,5 +11,14 @@ function adicionarAmigo (){
         input.focus();
     }
 
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = '';
+
+    for(let i = 0; i < amigos.length; i++){
+        let li = document.createElement('li');
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
+
     console.log(amigos);
 }
